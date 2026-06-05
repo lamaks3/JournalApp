73fb27d2c8406f441ae1871b45e4b7709082af35
Maksim Shyshko <lamakswork@gmail.com>
2026-06-05 13:42:50 +0300

    Initial commit

---
+++ SecretJournal/SecretJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretJournalApp.swift
+//  SecretJournal
+//
+//  Created by Maksim Shyshko on 05.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct SecretJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecretJournalTests/SecretJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecretJournalTests.swift
+//  SecretJournalTests
+//
+//  Created by Maksim Shyshko on 05.06.2026.
+//
+
+import Testing
+@testable import SecretJournal
+
+struct SecretJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
